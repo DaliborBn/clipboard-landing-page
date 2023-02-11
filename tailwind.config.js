0bea1c01415f4e6055d92547a8e64db1a9f28a4a
Dalibor Perić <109923493+DaliborBn@users.noreply.github.com>
2023-02-11 20:21:15 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bai: ['Bai Jamjuree']
+      },
+      screens: {
+        'sl': '481px'
+      },
+      colors: {
+        'strong-cyan': 'hsl(171, 66%, 44%)',
+        'light-blue': 'hsl(233, 100%, 69%)',
+        'dark-gb': 'hsl(210, 10%, 33%)',
+        'grayish-blue': 'hsl(201, 11%, 66%)',
+        'footer-bg': 'rgb(249, 246, 246)',
+        cyanShadow: 'hsl(171, 66%, 30%)',
+        blueShadow: 'hsl(233, 40%, 42%)'
+      },
+      boxShadow: {
+        mainShadow: '0 -3px inset'
+      }
+    },
+  },
+  plugins: [],
+}
